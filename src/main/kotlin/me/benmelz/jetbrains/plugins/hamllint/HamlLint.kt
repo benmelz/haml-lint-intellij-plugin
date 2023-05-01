688709ef4f1f9dde794784a24bfda81d97c71ab2
benmelz <ben@melz.me>
2023-05-01 17:45:44 -0400

    document HamlLint

--- src/main/kotlin/me/benmelz/jetbrains/plugins/hamllint/HamlLint.kt
+++ src/main/kotlin/me/benmelz/jetbrains/plugins/hamllint/HamlLint.kt
@@ -8,6 +8,13 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.Path
 import java.util.LinkedList
 
+/**
+ * Executes haml-lint externally using the command line and parses its output.
+ *
+ * @param[haml] the raw haml text to run against.
+ * @param[workDirectory] the work directory from which to run haml-lint.
+ * @return a list of collected haml-lint offenses.
+ */
 fun hamlLint(haml: CharSequence, workDirectory: Path): List<HamlLintOffense> {
     HamlLintTarget.createTempTarget(haml).use {
         val cli = hamlLintCommandLine(it, workDirectory)
@@ -15,6 +22,14 @@ fun hamlLint(haml: CharSequence, workDirectory: Path): List<HamlLintOffense> {
     }
 }
 
+/**
+ * Builds an executable [GeneralCommandLine] that runs `haml-lint` against a given [HamlLintTarget] within a given
+ * working directory within the context of `bundler`.
+ *
+ * @param[target] the [HamlLintTarget] to run against.
+ * @param[workDirectory] the directory from which to run `haml-lint`.
+ * @return an executable [GeneralCommandLine].
+ */
 private fun hamlLintCommandLine(target: HamlLintTarget, workDirectory: Path): GeneralCommandLine {
     return GeneralCommandLine("bundle").apply {
         this.addParameters("exec", "haml-lint", "--reporter", "json", target.absolutePath)
@@ -25,6 +40,12 @@ private fun hamlLintCommandLine(target: HamlLintTarget, workDirectory: Path): Ge
     }
 }
 
+/**
+ * Parses the output of a `haml-lint` run using the `json` reporter as [HamlLintOffense]s.
+ *
+ * @param[json] the raw output of a `haml-lint` run.
+ * @return a list of [HamlLintOffense]s.
+ */
 private fun parseHamlLintOutput(json: String): List<HamlLintOffense> {
     val sanitizedJson = sanitizeHamlLintOutput(json) ?: return emptyList()
     val filesArray = JsonParser.parseString(sanitizedJson).asJsonObject["files"].asJsonArray
@@ -44,6 +65,12 @@ private fun parseHamlLintOutput(json: String): List<HamlLintOffense> {
     }
 }
 
+/**
+ * Sanitizes the output of a `haml-lint` run using the `json` reporter by removing leading non-`{` characters.
+ *
+ * @param[json] the unsanitized `json` string.
+ * @return a sanitized `json` string.
+ */
 private fun sanitizeHamlLintOutput(json: String): String? {
     val jsonStartIndex = json.indexOf('{')
     return if (jsonStartIndex == -1) null else json.substring(jsonStartIndex)
