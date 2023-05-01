8337a03f5728ccfc183050d96711e559339b458e
benmelz <ben@melz.me>
2023-05-01 17:21:52 -0400

    document HamlLintTarget

--- src/main/kotlin/me/benmelz/jetbrains/plugins/hamllint/HamlLintTarget.kt
+++ src/main/kotlin/me/benmelz/jetbrains/plugins/hamllint/HamlLintTarget.kt
@@ -6,12 +6,22 @@ import java.io.File
 import java.io.FileWriter
 import java.net.URI
 
+/**
+ * A file containing `haml` code that is deleted once closed.
+ */
 class HamlLintTarget(uri: URI) : File(uri), AutoCloseable {
+    /**
+     * Deletes this target.
+     */
     override fun close() {
         FileUtilRt.delete(this)
     }
 
     companion object {
+        /**
+         * @param[haml] the `haml` code to be written.
+         * @return a temporary [HamlLintTarget] containing the given `haml` code.
+         */
         fun createTempTarget(haml: CharSequence): HamlLintTarget {
             val tempTarget = FileUtilRt.createTempFile("haml_lint_target", null, true)
             BufferedWriter(FileWriter(tempTarget)).use { it.write(haml.toString()) }
