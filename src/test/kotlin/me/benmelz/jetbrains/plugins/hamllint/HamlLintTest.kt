d92471d89d7f1b55015916bc169a92cbc2376851
benmelz <ben@melz.me>
2025-01-15 15:59:25 -0500

    fix mocking

--- src/test/kotlin/me/benmelz/jetbrains/plugins/hamllint/HamlLintTest.kt
+++ src/test/kotlin/me/benmelz/jetbrains/plugins/hamllint/HamlLintTest.kt
@@ -1,9 +1,11 @@
 package me.benmelz.jetbrains.plugins.hamllint
 
+import com.intellij.execution.process.OSProcessHandler
 import com.intellij.execution.process.ScriptRunnerUtil
+import java.nio.file.Path
 import org.mockito.Mockito.mockStatic
 import org.mockito.kotlin.any
-import java.nio.file.Path
+import org.mockito.kotlin.isA
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -18,10 +20,15 @@ internal class HamlLintTest {
         mockStatic(ScriptRunnerUtil::class.java).use { mockedScriptRunnerUtil ->
             mockedScriptRunnerUtil
                 .`when`<Any> {
-                    ScriptRunnerUtil.getProcessOutput(any())
+                    ScriptRunnerUtil.getProcessOutput(isA<OSProcessHandler>(), any(), any())
                 }.thenReturn(hamlLintOutputJson)
 
-            val offenses = hamlLint("test haml", Path.of("/"), listOf("bundle", "exec", "haml-lint"))
+            val offenses = hamlLint(
+                "test haml",
+                Path.of("test.html.haml"),
+                Path.of("/"),
+                listOf("bundle", "exec", "haml-lint")
+            )
             assertEquals(offenses.size, 3)
             assertEquals(offenses[0], HamlLintOffense(0, "warning", "TestOffense0", "test offense 0"))
             assertEquals(offenses[1], HamlLintOffense(1, "warning", "TestOffense1", "test offense 1"))
