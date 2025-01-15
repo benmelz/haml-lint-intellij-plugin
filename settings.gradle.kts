e5c0b471ee7352489e8dd585fae63842180bae6c
benmelz <ben@melz.me>
2025-01-15 13:57:41 -0500

    copy over plugin template files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,5 @@
-rootProject.name = "haml-lint-intellij-plugin"
+rootProject.name = "IntelliJ Platform Plugin Template"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
