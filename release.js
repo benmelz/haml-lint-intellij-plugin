863ebe1ad8d491213330a6e066bbfefabb874fcb
benmelz <ben@melz.me>
2025-04-09 15:12:52 -0400

    chore: get releases working again, hopefully

--- release.js
+++ release.js
@@ -5,7 +5,10 @@ const exec = util.promisify(require('child_process').exec);
 module.exports = {
   prepare: async (config, context) => {
     const prevProperties = fs.readFileSync('./gradle.properties', { encoding: 'UTF-8' });
-    const nextProperties = prevProperties.replace(/pluginVersion=.+/, `pluginVersion=${context.nextRelease.version}`);
+    const nextProperties = prevProperties.replace(
+        /pluginVersion = .+/,
+        `pluginVersion = ${context.nextRelease.version}`,
+    );
     fs.writeFileSync('./gradle.properties', nextProperties);
     const { stdout, stderr } = await exec('./gradlew signPlugin');
     console.log(stdout);
